64bd2fa6c0da3bbc6834bf62cd55f415a6c4df1d
Adam Ahrens <adam.aahrens@gmail.com>
2021-01-25 11:23:26 -0600

    Adding initial day16 app

---
+++ day16/Day16.md
@@ -0,0 +1,3 @@
+# Day 16
+
+hackingwithswift.com/100/swiftui/16
---
+++ day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Adam Ahrens on 1/25/21.
+//  Copyright © 2021 Adam Ahrens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
