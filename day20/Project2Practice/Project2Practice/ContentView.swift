0e3132c46896df01a7a4262ffe6a67bc94602053
Adam Ahrens <adam.aahrens@gmail.com>
2021-01-28 10:17:45 -0600

    Adding missing file

--- day20/Project2Practice/Project2Practice/ContentView.swift
+++ day20/Project2Practice/Project2Practice/ContentView.swift
@@ -9,10 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  var body: some View {
+    // Build a Grid
+    HStack(alignment: .center, spacing: 20) {
+      VStack {
+        Text("1")
+        Text("4")
+        Text("7")
+      }
+      VStack {
+        Text("2")
+        Text("5")
+        Text("8")
+      }
+      VStack {
+        Text("3")
+        Text("6")
+        Text("9")
+      }
     }
+  }
 }
+
+/*
+ var body: some View {
+ VStack {
+ VStack(alignment: .center, spacing: 12.0) {
+ Text("Top VStack")
+ Text("Bottom VStack")
+ }
+ 
+ Spacer()
+ 
+ HStack(alignment: .top, spacing: 12.0) {
+ Text("Left HStack")
+ Text("Right HStack")
+ }
+ 
+ Spacer()
+ 
+ ZStack {
+ Text("Behind ZStack")
+ Text("Front ZStack")
+ }
+ }
+ }
+ */
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
