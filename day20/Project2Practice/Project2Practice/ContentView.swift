0f44b0e39b6c852c3bdc35f943512c93fa7941a6
Adam Ahrens <adam.aahrens@gmail.com>
2021-01-28 10:17:37 -0600

    Playing with VStack, HStack, and ZStack

---
+++ day20/Project2Practice/Project2Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2Practice
+//
+//  Created by Adam Ahrens on 1/28/21.
+//  Copyright © 2021 Adam Ahrens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
