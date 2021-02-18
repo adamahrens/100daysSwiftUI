5ab68466172735201833f4bbc1d8a8003ba7d050
Adam Ahrens <adam.aahrens@gmail.com>
2021-02-18 08:26:39 -0600

    Adding Day26 Starter Project

---
+++ day26/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Adam Ahrens on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
