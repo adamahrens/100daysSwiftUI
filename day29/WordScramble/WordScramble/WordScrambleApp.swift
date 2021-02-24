1951695096dcb31481f6353de734badd44ffe7a4
Adam Ahrens <adam.aahrens@gmail.com>
2021-02-24 16:10:41 -0600

    Adding initial day29 app

---
+++ day29/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Adam Ahrens on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
