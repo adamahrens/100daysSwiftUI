17bcc6b781ef151465b28d794d76616b49302fe6
Adam Ahrens <adam.aahrens@gmail.com>
2021-03-01 14:05:35 -0600

    Adding starter code for day35 project

---
+++ day35/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Adam Ahrens on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
