3439988071eb2e609ad549f913ee7c3ba2d6351e
Adam Ahrens <adam.aahrens@gmail.com>
2021-03-01 11:05:06 -0600

    Adding Animations App

---
+++ day32/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Adam Ahrens on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
