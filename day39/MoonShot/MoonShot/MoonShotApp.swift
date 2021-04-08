6c5fc5ff97a2a82489f75fd44caf4426328f39ed
Adam Ahrens <adam.aahrens@gmail.com>
2021-04-08 09:45:27 -0500

    Adding new day39 project

---
+++ day39/MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Adam Ahrens on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
